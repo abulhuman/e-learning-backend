349e1369cd9dcee1aacf2f11bed369f164959259
Ezra Ashenafi <eazash22@gmail.com>
2022-03-29 14:20:58 +0300

    chore: update type annotation for validate

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common'
 import * as bcrypt from 'bcrypt'
+import { User } from 'src/users/entities/user.entity'
 import { UsersService } from 'src/users/users.service'
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
-  async validateUser(email: string, password: string): Promise<any> {
+  async validateUser(email: string, password: string): Promise<User | null> {
     const user = await this.userService.findByEmail(email)
     if (user && (await bcrypt.compare(password, user.password))) {
       return user
