8e6baddb0d106538f9563b48f23253b3a8a6ef93
Ezira Ashenafi <eazash22@gmail.com>
2022-05-04 10:09:09 +0300

    feat: email verification link on account creation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,28 @@
-import { Injectable } from '@nestjs/common'
+import { forwardRef, Inject, Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 import * as bcrypt from 'bcrypt'
+import { MailService } from 'src/mail/mail.service'
 import { User } from 'src/users/entities/user.entity'
 import { UsersService } from 'src/users/users.service'
-
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    @Inject(forwardRef(() => UsersService))
+    private userService: UsersService,
+    private jwtService: JwtService,
+    private mailService: MailService,
+  ) {}
   async validateUser(email: string, password: string): Promise<User | null> {
     const user = await this.userService.findOneUserByEmail(email)
     if (user && (await bcrypt.compare(password, user.password))) {
       return user
     }
     return null
+  }
+  async sendVerificationLink(user: User) {
+    const payload = { id: user.id }
+    const token = this.jwtService.sign(payload)
+    const name = [user.firstName, user.lastName].join(' ')
+    await this.mailService.sendVerificationEmail(user.email, token, name)
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common'
+import { forwardRef, Module } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { UserResolver, RoleResolver } from './users.resolver'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './entities/user.entity'
 import { Role } from './entities/role.entity'
+import { AuthModule } from 'src/auth/auth.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Role])],
+  imports: [
+    TypeOrmModule.forFeature([User, Role]),
+    forwardRef(() => AuthModule),
+  ],
   providers: [UserResolver, RoleResolver, UsersService],
   exports: [UsersService],
 })
