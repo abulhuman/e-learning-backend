5f0570ea82192abdcef7ee347f73a389f4ec91aa
Adem Mohammed <adem164416@gmail.com>
2022-05-23 22:48:22 +0300

    fix: create student class by department id

--- src/users/dto/create-student-class.input.ts
+++ src/users/dto/create-student-class.input.ts
@@ -1,7 +1,11 @@
-import { IsNotEmpty, IsString } from 'class-validator'
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator'
 import { StudentClass } from '../entities/student-class.entity'
+import { CreateStudentClassInput as ICreateStudentClassInput } from 'src/graphql'
 
-export class CreateStudentClassInput extends StudentClass {
+export class CreateStudentClassInput
+  extends StudentClass
+  implements ICreateStudentClassInput
+{
   @IsNotEmpty()
   @IsString()
   year: string
@@ -9,4 +13,8 @@ export class CreateStudentClassInput extends StudentClass {
   @IsNotEmpty()
   @IsString()
   section: string
+
+  @IsNotEmpty()
+  @IsUUID()
+  departmentId: string
 }
