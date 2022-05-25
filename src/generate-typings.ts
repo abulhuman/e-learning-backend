95183654e755e7bab7acb387f77f14baf961226f
Adem Mohammed <adem164416@gmail.com>
2022-05-25 17:51:52 +0300

    fix: set ts-schema watch mode based on cli argv[2]

--- src/generate-typings.ts
+++ src/generate-typings.ts
@@ -2,8 +2,16 @@ import { GraphQLDefinitionsFactory } from '@nestjs/graphql'
 import { join } from 'path'
 
 const definitionsFactory = new GraphQLDefinitionsFactory()
+process.argv[2] = process.argv[2] ?? false.toString()
+let watch: true | false
+try {
+  watch = JSON.parse(process.argv[2])
+} catch (error) {
+  watch = false
+}
+
 definitionsFactory.generate({
   typePaths: ['./src/**/*.graphql'],
   path: join(process.cwd(), 'src/graphql.ts'),
-  watch: true,
+  watch,
 })
