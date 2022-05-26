f7eb3ca7d765daadd72628cdf845e01cc92eceee
Adem Mohammed <adem164416@gmail.com>
2022-05-26 16:51:47 +0300

    fix: set ID datatypes to update inputs

--- src/course/dto/update-course.input.ts
+++ src/course/dto/update-course.input.ts
@@ -1,6 +1,9 @@
 import { CreateCourseInput } from './create-course.input'
 import { PartialType } from '@nestjs/mapped-types'
+import { IsNotEmpty, IsUUID } from 'class-validator'
 
 export class UpdateCourseInput extends PartialType(CreateCourseInput) {
+  @IsNotEmpty()
+  @IsUUID()
   id: string
 }
