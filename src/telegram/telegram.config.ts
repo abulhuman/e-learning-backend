7533f1a1713d3e56f7884807ce7df9baf126cb8d
Ezira Ashenafi <eazash22@gmail.com>
2022-05-27 13:41:18 +0300

    feat: add file endpoints

--- src/telegram/telegram.config.ts
+++ src/telegram/telegram.config.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi'
 
 export const telegramConfigValidation = {
   BOT_KEY: Joi.string().required(),
-  TELEGRAM_API_URL: Joi.string().default('https://api.telegram.org/bot'),
+  TELEGRAM_API_URL: Joi.string().default('https://api.telegram.org'),
   HEROKU_URL: Joi.string().when('NODE_ENV', {
     is: 'production',
     then: Joi.required(),
