83b239c7a0305bc96972871f25ab6db3398fca92
Ezira Ashenafi <eazash22@gmail.com>
2022-06-19 16:14:07 +0300

    feat: filter out late assingments

--- src/telegram/handlers/command/submit.command.handler.ts
+++ src/telegram/handlers/command/submit.command.handler.ts
@@ -1,9 +1,9 @@
 import { forwardRef, Inject, Injectable, Logger } from '@nestjs/common'
-import { from, switchMap } from 'rxjs'
+import { from, map, switchMap } from 'rxjs'
 import { InlineKeybardMarkup, TextMessage } from 'src/telegram/dtos'
 import { Handler } from 'src/telegram/interfaces/hanlder.interface'
 import { TelegramService } from 'src/telegram/telegram.service'
-
+import * as moment from 'moment'
 @Injectable()
 export class SubmitCommandHandler implements Handler {
   logger = new Logger(SubmitCommandHandler.name)
@@ -17,13 +17,18 @@ export class SubmitCommandHandler implements Handler {
         switchMap(account =>
           this.telegramService.findAssignmentsForUser(account.user.id),
         ),
+        map(assignments =>
+          assignments.filter(assignment => {
+            moment(assignment.submissionDeadline).diff(moment()) > 0
+          }),
+        ),
         switchMap(assignments => {
           let text: string
           const buttons: InlineKeybardMarkup = {
             inline_keyboard: [],
           }
           if (!assignments.length) {
-            text = 'No assignments'
+            text = 'No assignments yet'
           } else {
             text = 'Select Assignment'
             assignments.forEach(assignment => {
